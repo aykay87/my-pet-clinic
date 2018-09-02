7b10484c7973afaa497999b9361c2879fa8ba6d1
alex <it-architektur@alexander-kaden.de>
2018-09-02 16:01:36 +0200

    refactoring to common service interface. closes #11

--- my-pet-clinic-data/src/main/java/de/alexanderkaden/mypetclinic/services/PetService.java
+++ my-pet-clinic-data/src/main/java/de/alexanderkaden/mypetclinic/services/PetService.java
@@ -3,13 +3,6 @@ package de.alexanderkaden.mypetclinic.services;
 
 import de.alexanderkaden.mypetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- my-pet-clinic-data/src/main/java/de/alexanderkaden/mypetclinic/services/VetService.java
+++ my-pet-clinic-data/src/main/java/de/alexanderkaden/mypetclinic/services/VetService.java
@@ -3,13 +3,7 @@ package de.alexanderkaden.mypetclinic.services;
 
 import de.alexanderkaden.mypetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
