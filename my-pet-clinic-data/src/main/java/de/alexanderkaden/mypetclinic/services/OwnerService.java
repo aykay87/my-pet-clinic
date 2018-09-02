577b7fd2a561a3c9c27a86d90d3bb2a13e1909d9
alex <it-architektur@alexander-kaden.de>
2018-09-02 15:39:58 +0200

    my-pet-clinic-5 Create Interfaces for Pet Clinic Services . closes #5

---
+++ my-pet-clinic-data/src/main/java/de/alexanderkaden/mypetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package de.alexanderkaden.mypetclinic.services;
+
+import de.alexanderkaden.mypetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
